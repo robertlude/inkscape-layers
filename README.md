a6d6639c750ec7cbcc6ed71cc100392d546a4f4d
Robert Lude <robert.lude@gmail.com>
2014-07-12 11:10:43 -0400

    Added output directory option

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Splits Inkscape SVG layers into individual SVG documents
 
 ## Usage
 
-`inkscape-layers input_svg`
+`inkscape-layers input_svg [-o output_directory]`
 
 Examines `input_svg` for each Inkscape layer and exports them to the same directory as `input_svg` with "-" and the layer's name appended to the end before the file extension.
+
+If `output_directory` is specified, the files will be placed in that directory.
 
 For example, `inkscape-layers test.svg` will produce `test-1.svg`, `test-2.svg`, `test-3.svg` because the included `test.svg` has three layers, named "1", "2", and "3".
 
--- inkscape-layers.cpp
+++ inkscape-layers.cpp
@@ -1,12 +1,23 @@
 
+/////////////////////////////////////
+//                          _____  //
+//  inkscape-layers        /    /  //
+//                        /____//  //
+//  by Robert Lude        /____//  //
+//    <rob@ertlu.de>      /____/   //
+//                                 //
+/////////////////////////////////////
+
+
 #include <libxml++/libxml++.h>
 #include <iostream>
+#include <string.h>
 #include <stdlib.h>
 
   void
 printUsage
   (  )
-  { std::cout << "Usage: inkscape-layers inputfile" << std::endl; }
+  { std::cout << "Usage: inkscape-layers input_file [-o output_directory]" << std::endl; }
 
   std::string
 getFilepathBase
@@ -31,7 +42,7 @@ getFilepathBase
   std::vector<std::string>*
 acquireLayerNames
   (           xmlpp::Document*  document
-  ,  xmlpp::Node::PrefixNsMap*  prefixNsMap
+  ,  xmlpp::Node::PrefixNsMap&  prefixNsMap
   )
   { ;; /* Local Variables */ ;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;                 xmlpp::Element*  rootNode       ;
@@ -43,7 +54,7 @@ acquireLayerNames
 
     rootNode = document->get_root_node();
 
-    layers = rootNode->find("//svg:g[@inkscape:groupmode='layer']", *prefixNsMap);
+    layers = rootNode->find("//svg:g[@inkscape:groupmode='layer']", prefixNsMap);
 
     result = new std::vector<std::string>();
 
@@ -61,14 +72,14 @@ acquireLayerNames
 isolate
   (                std::string  layerName
   ,            xmlpp::Element*  rootNode
-  ,  xmlpp::Node::PrefixNsMap*  prefixNsMap
+  ,  xmlpp::Node::PrefixNsMap&  prefixNsMap
   )
   { ;; /* Local Variables */ ;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;                  xmlpp::NodeSet  layers         ;
     ;  xmlpp::NodeSet::const_iterator  layerIterator  ;
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    layers = rootNode->find(std::string("//svg:g[@inkscape:label != '") + layerName + "']", *prefixNsMap);
+    layers = rootNode->find(std::string("//svg:g[@inkscape:label != '") + layerName + "']", prefixNsMap);
 
     layerIterator = layers.begin();
     while ( layerIterator != layers.end() )
@@ -83,7 +94,7 @@ main
   ,  char**  argument
   )
   { ;; /* Local Variables */ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ;                   xmlpp::DomParser*  domParser            ;
+    ;                    xmlpp::DomParser  domParser            ;
     ;           std::vector<std::string>*  layerNames           ;
     ;  std::vector<std::string>::iterator  layerIterator        ;
     ;                         std::string  inputFilename        ;
@@ -91,52 +102,102 @@ main
     ;                         std::string  layerFilenameSuffix  ;
     ;              std::string::size_type  pathEnd              ;
     ;              std::string::size_type  extensionStart       ;
-    ;           xmlpp::Node::PrefixNsMap*  prefixNsMap          ;
+    ;            xmlpp::Node::PrefixNsMap  prefixNsMap          ;
     ;                         std::string  layerOutputFilename  ;
     ;                    xmlpp::Document*  document             ;
+    ;                         std::string  outputDirectory      ;
+    ;                         std::string  outputBaseName       ;
+    ;                                 int  argumentIndex        ;
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    if ( argumentCount != 2 )
+    // Make sure we have command line arguments
+
+    if ( argumentCount < 2 )
        { printUsage();
          return -1;
        }
 
-    inputFilename = argument[1];
+    // Read command line arguments
 
-    domParser = new xmlpp::DomParser(inputFilename);
-    document = domParser->get_document();
+    for ( argumentIndex = 1 ; argumentIndex < argumentCount ; ++argumentIndex )
+        { if ( strcmp(argument[argumentIndex], "-o") == 0 )
+             { if ( argumentIndex + 1 == argumentCount )
+                  { printUsage();
+                    return -1;
+                  }
 
-    prefixNsMap = new xmlpp::Node::PrefixNsMap();
-    (*prefixNsMap)["svg"] = "http://www.w3.org/2000/svg";
-    (*prefixNsMap)["inkscape"] = "http://www.inkscape.org/namespaces/inkscape";
+               if ( outputDirectory.length() != 0 )
+                  { printUsage();
+                    return -1;
+                  }
+
+               outputDirectory = argument[argumentIndex + 1];
+               if ( outputDirectory[outputDirectory.length()] != '/' ) outputDirectory += '/';
+
+               ++argumentIndex;
+               continue;
+             }
+
+          if ( inputFilename.length() != 0 )
+             { printUsage();
+               return -1;
+             }
+
+          inputFilename = argument[argumentIndex];
+        }
+
+    // Setup XML namespaces
+
+    prefixNsMap["svg"] = "http://www.w3.org/2000/svg";
+    prefixNsMap["inkscape"] = "http://www.inkscape.org/namespaces/inkscape";
+
+    // Read layer names
+
+    domParser.parse_file(inputFilename);
+    document = domParser.get_document();
 
     layerNames = acquireLayerNames(document, prefixNsMap);
+
+    // Setup filename variables
 
     pathEnd = inputFilename.rfind('/');
     if ( pathEnd == std::string::npos ) pathEnd = 0;
     extensionStart = inputFilename.find('.', pathEnd);
 
+    if ( outputDirectory.length() == 0 ) outputDirectory = inputFilename.substr(0, pathEnd);
+
+    outputBaseName = inputFilename.substr(pathEnd, extensionStart - pathEnd);
+    layerFilenamePrefix = outputDirectory + outputBaseName;
     if ( extensionStart == std::string::npos ) {
-      layerFilenamePrefix = inputFilename;
       layerFilenameSuffix = "";
     } else {
-      layerFilenamePrefix = inputFilename.substr(0, extensionStart);
       layerFilenameSuffix = inputFilename.substr(extensionStart);
     }
 
+    /*std::cout << "pathEnd: " << pathEnd << std::endl;
+    std::cout << "extensionStart: " << extensionStart << std::endl;
+    std::cout << "outputDirectory: " << outputDirectory << std::endl;
+    std::cout << "outputBaseName: " << outputBaseName << std::endl;
+    std::cout << "layerFilenamePrefix: " << layerFilenamePrefix << std::endl;
+    std::cout << "layerFilenameSuffix: " << layerFilenameSuffix << std::endl;*/
+
+    // Extract each layer
+
     layerIterator = layerNames->begin();
     while ( layerIterator != layerNames->end() )
-          { domParser->parse_file(inputFilename);
+          { domParser.parse_file(inputFilename);
             layerOutputFilename = layerFilenamePrefix + "-" + (*layerIterator) + layerFilenameSuffix;
-            document = domParser->get_document();
+            document = domParser.get_document();
             isolate(*layerIterator, document->get_root_node(), prefixNsMap);
             document->write_to_file(layerOutputFilename);
             ++layerIterator;
           }
 
-    delete prefixNsMap;
+    // Clean up
+
     delete layerNames;
-    delete domParser;
+
+    // Finish
 
     return 0;
   }
